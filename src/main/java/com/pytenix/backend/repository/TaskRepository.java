78ed666c41beb277a477beda3c3bcc7169bb597b
BABABABA <steffen.flaskamp@bk-beckum.eu>
2026-05-11 21:54:45 +0200

    added TaskRepository to manage Task

---
+++ src/main/java/com/pytenix/backend/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.pytenix.backend.repository;
+
+import com.pytenix.backend.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
