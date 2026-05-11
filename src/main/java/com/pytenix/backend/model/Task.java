2991a30e3c5470458296d05b1bd299bb11ddb61d
BABABABA <steffen.flaskamp@bk-beckum.eu>
2026-05-11 21:53:40 +0200

    fixxing faulty dependencies in pom.xml added Task objective

---
+++ src/main/java/com/pytenix/backend/model/Task.java
@@ -0,0 +1,21 @@
+package com.pytenix.backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+}
